c3d71d875b42db1055b867013c7eb7924f0b9ff9
Mohan Dwivedi <manmohandwivedi@yahoo.com>
2025-03-03 23:43:03 +0530

    update code

--- src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        //next is the flage to tell the funtio to use the middleware
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-    }
-}
-export {asyncHandler}
